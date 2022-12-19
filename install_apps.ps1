d43cbea45d46b6e0694549b8ff5952a47565b717
advornicov <114244455+advornicov@users.noreply.github.com>
2022-12-19 12:11:05 -0500

    Update install_apps.ps1

--- install_apps.ps1
+++ install_apps.ps1
@@ -3,6 +3,7 @@
 ### Define variables for Python   ###
 $url_python = "https://www.python.org/ftp/python/3.11.1/python-3.11.1-amd64.exe"
 $output_python = "C:/temp/python-3.11.1-amd64.exe"
+$install_python_log = "C:/temp/Python3.11.1-Install.log"
 
 ### Create a local directory called temp  ###
 New-Item -ItemType Directory -Force -Path C:/temp
@@ -14,25 +15,30 @@ New-Item -ItemType Directory -Force -Path C:/temp
 Invoke-WebRequest -Uri $url_python -OutFile $output_python
 
 ### Install Python application in a quiet mode ###
-& $output_python /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 
+& $output_python /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 /log "$install_python_log"
 
 #################################################################################################
 
-### INSTALL VIM 9.0 ###
+### INSTALL GIT 2.39 ###
 
-### Define variables for VIM   ###
-$url_vim = "https://github.com/vim/vim-win32-installer/releases/download/v9.0.1047/gvim_9.0.1047_x64_signed.exe"
-$output_vim = "C:/temp/gvim_9.0.1047_x64_signed.exe"
+### Define variables   ###
+$url_git = "https://github.com/git-for-windows/git/releases/download/v2.39.0.windows.1/Git-2.39.0-64-bit.exe"
+$output_git = "C:/temp/Git-2.39.0-64-bit.exe"
+$install_git_log = "C:/temp/Git2.39-Install.log"
 
+### Create a local directory called temp  ###
+New-Item -ItemType Directory -Force -Path C:/temp
 
 ### Force Powershell to use TLS 1.2   ###
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-### Download Vim executable ###
-Invoke-WebRequest -Uri $url_vim -OutFile $output_vim
+### Download Git executable ###
+Invoke-WebRequest -Uri $url_git -OutFile $output_git
 
-### Install Vim application in a quiet mode ###
-& $output_vim /S
+### Install Git application in a quiet mode ###
+& $output_git /VERYSILENT /NORESTART /COMPONENTS=icons,icons\desktop,ext,ext\shellhere,ext\guihere,gitlfs,assoc,assoc_sh /LOG="$install_git_log" 
+
+
 
 ##################################################################################################
 
